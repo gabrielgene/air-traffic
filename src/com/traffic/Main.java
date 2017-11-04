119e3673663becd635d8638bf7af93934b484906
Gabriel Genê <gabrielgene97@gmail.com>
2017-11-04 15:52:46 -0300

    Implement main

--- src/com/traffic/Main.java
+++ src/com/traffic/Main.java
@@ -1,8 +1,18 @@
 package com.traffic;
 
+import com.data.Flight;
+import com.util.Util;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Air Traffic Controll Simulator");
+
+        final Flight flight1 = new Flight(Util.getRandomStatus(), Util.getRandomModel());
+        final Flight flight2 = new Flight(Util.getRandomStatus(), Util.getRandomModel());
+
+
+        flight1.about();
+        flight2.about();
     }
 }
