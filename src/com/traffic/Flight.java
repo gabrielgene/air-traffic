21cc428c634a2ce2cfe7d616f10b40c9490ee27a
Gabriel Genê <gabrielgene97@gmail.com>
2017-11-03 23:59:14 -0300

    Initial commit

---
+++ src/com/traffic/Flight.java
@@ -0,0 +1,19 @@
+package com.traffic;
+
+import java.util.Date;
+
+public class Flight {
+    String flightStatus;
+    String modelo;
+    Date factoryDate;
+    Integer actualSpeed;
+    Integer actualAltitude;
+    String direction;
+    String transporter;
+    Date startDate;
+    Date endDate;
+    String destine;
+    String origin;
+
+}
+
