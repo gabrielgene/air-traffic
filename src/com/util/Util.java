b84519601b90d7a752ba719d4f3177c3418e27e3
Gabriel Genê <gabrielgene97@gmail.com>
2017-11-05 20:50:48 -0300

    Change data type to array

--- src/com/util/Util.java
+++ src/com/util/Util.java
@@ -3,35 +3,36 @@ package com.util;
 import com.data.FlightStatus;
 import com.data.Model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
 public class Util {
 
-    private static Object getRandomValueFromMap(final HashMap map) {
+    private static Object getRandomValueFromMap(final ArrayList list) {
         final Random generator = new Random();
-        final Object[] values = map.values().toArray();
+        final Object[] values = list.toArray();
         return values[generator.nextInt(values.length)];
     }
 
     public static FlightStatus getRandomStatus() {
-        final HashMap<Object, Object> status = new HashMap<>();
-        status.put(0, new FlightStatus("Preflight", "Takeoff")); //Esta é a parte do vôo em que o avião ainda está no chão
-        status.put(1, new FlightStatus("Takeoff", "Departure")); //O piloto acelera a aeronave na pista
-        status.put(2, new FlightStatus("Departure", "En route"));//O avião levanta do chão e sobe para uma altitude consideravel
-        status.put(3, new FlightStatus("En route", "Descent"));//A aeronave viaja através de um ou mais espaços aéreos
-        status.put(4, new FlightStatus("Descent", "Approach"));//O piloto desce e manobra para o aeroporto de destino
-        status.put(5, new FlightStatus("Approach", "Landing"));//O piloto alinha a aeronave com a pista de pouso designada
-        status.put(6, new FlightStatus("Landing", ""));//A aeronae aterra na pista e para no terminal de destino.
+        final ArrayList<FlightStatus> status = new ArrayList<>();
+        status.add(new FlightStatus("Preflight", "Takeoff")); //Esta é a parte do vôo em que o avião ainda está no chão
+        status.add(new FlightStatus("Takeoff", "Departure")); //O piloto acelera a aeronave na pista
+        status.add(new FlightStatus("Departure", "En route"));//O avião levanta do chão e sobe para uma altitude consideravel
+        status.add(new FlightStatus("En route", "Descent"));//A aeronave viaja através de um ou mais espaços aéreos
+        status.add(new FlightStatus("Descent", "Approach"));//O piloto desce e manobra para o aeroporto de destino
+        status.add(new FlightStatus("Approach", "Landing"));//O piloto alinha a aeronave com a pista de pouso designada
+        status.add(new FlightStatus("Landing", ""));//A aeronae aterra na pista e para no terminal de destino.
         return (FlightStatus) getRandomValueFromMap(status);
     }
 
     public static Model getRandomModel() {
-        final HashMap<Object, Object> models = new HashMap<>();
-        models.put(0, new Model("ATR 42-500", "20/02/2014"));
-        models.put(1, new Model("Airbus A320", "17/04/2013"));
-        models.put(2, new Model("Embraer 175", "03/08/2015"));
-        models.put(3, new Model("Boeing 737-700", "30/06/2017"));
+        final ArrayList<Model> models = new ArrayList<>();
+        models.add(new Model("ATR 42-500", "20/02/2014"));
+        models.add(new Model("Airbus A320", "17/04/2013"));
+        models.add(new Model("Embraer 175", "03/08/2015"));
+        models.add(new Model("Boeing 737-700", "30/06/2017"));
         return (Model) getRandomValueFromMap(models);
     }
 }
