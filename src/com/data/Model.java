5d3addb522eb52a0a63246f1e6a0710d12364574
Gabriel Genê <gabrielgene97@gmail.com>
2017-11-15 03:44:48 -0300

    Implement model from flights

--- src/com/data/Model.java
+++ src/com/data/Model.java
@@ -1,20 +1,42 @@
 package com.data;
 
+import com.util.Util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Model {
-    private final String model;
-    private final String factoryDate;
+  private final String name;
+  private final String fabricated;
+  private static final ArrayList<Model> models;
 
-    public Model(final String model, final String factoryDate) {
-        this.model = model;
-        this.factoryDate = factoryDate;
-    }
+  static {
+    models = new ArrayList<>(Arrays.asList(
+            new Model("ATR 42-500", "20/02/2014"),
+            new Model("Airbus A320", "17/04/2013"),
+            new Model("Embraer 175", "03/08/2015"),
+            new Model("Boeing 737-700", "30/06/2017")
+    ));
+  }
 
-    public String getModel() {
-        return model;
-    }
+  private Model(final String name, final String fabricated) {
+    this.name = name;
+    this.fabricated = fabricated;
+  }
 
-    public String getFactoryDate() {
-        return factoryDate;
-    }
+  public String getName() {
+    return name;
+  }
+
+  public String getFabricated() {
+    return fabricated;
+  }
+
+  public static int getModelsSize() {
+    return models.size();
+  }
+
+  public static Model getModel(final Integer index) {
+    return models.get(index);
+  }
 }
