48ffda7981440195b8eff3166d138c22278d6ca2
Gabriel Genê <gabrielgene97@gmail.com>
2017-11-15 03:44:08 -0300

    Implement flight type

--- src/com/data/Flight.java
+++ src/com/data/Flight.java
@@ -1,25 +1,48 @@
 package com.data;
 
+
+/* models
+
+ */
+
+/* fabricated
+
+ */
+
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import com.util.Util;
+
 public class Flight {
-    FlightStatus flightStatus;
-    final Model model;
 
-    public Flight(FlightStatus flightStatus, final Model model) {
-        this.flightStatus = flightStatus;
-        this.model = model;
-    }
+  private Integer status;
+  final private Model model;
+  private String speed;
+  private String altitude;
+  private String direction;
+  private String transporter;
+  private String goingHour;
+  private String arriveHour;
+  private String origin;
+  private String destiny;
 
-    public void getStatus() {
-        System.out.println(this.flightStatus);
-    }
+  public Flight() {
+    this.status = Util.getRandomNumber(Status.getStatusSize());
+    this.model = Model.getModel(Util.getRandomNumber(Model.getModelsSize()));
+  }
 
-    public void about() {
-        System.out.println(String.format(
-                "Airplane model %s fabricated in %s, %s",
-                this.model.getModel(),
-                this.model.getFactoryDate(),
-                this.flightStatus.getStatus())
-        );
+  public void nextStatus() throws Exception {
+    this.status++;
+    if (this.status > 6) {
+      throw new Exception("Aeronave já está aterrada");
+    } else {
+      about();
     }
+  }
+
+  public void about () {
+    String modelName = model.getName();
+    String fabricated = model.getFabricated();
+    System.out.format("Avião modelo: %s, fabricado: %s, status: %s \n", modelName, fabricated, Status.getStatus(status));
+  }
 }
 
